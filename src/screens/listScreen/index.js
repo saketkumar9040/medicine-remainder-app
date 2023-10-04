9c0e39220b4d36940be61773f8711e0a45424f74
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-04 16:40:53 +0530

    feat - started with lists screen

--- src/screens/listScreen/index.js
+++ src/screens/listScreen/index.js
@@ -2,12 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react';
 
 import styles from './style';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ListScreen = () => {
   return (
-    <View>
-      <Text>ListScreen</Text>
-    </View>
+<SafeAreaView style={styles.mainContainer}>
+
+</SafeAreaView>
   )
 }
 
--- src/screens/listScreen/style.js
+++ src/screens/listScreen/style.js
@@ -1,7 +1,11 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
+    mainContainer:{
+        flex:1,
+        paddingTop:Platform.OS ==="android"?40:0,
+        backgroundColor:"#000"
+    }
 });
 
 export default styles;
