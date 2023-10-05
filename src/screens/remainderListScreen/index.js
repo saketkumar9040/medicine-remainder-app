1cf1a47e1dfeeb5bb0863b77dd2e9f80a7ed596a
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-06 01:15:57 +0530

    style - started styling doses list screen

--- src/screens/remainderListScreen/index.js
+++ src/screens/remainderListScreen/index.js
@@ -1,14 +1,25 @@
-import { View, Text ,SafeAreaView} from 'react-native'
-import React from 'react';
+import { View, Text, SafeAreaView, Image ,TouchableOpacity} from "react-native";
+import React from "react";
+import { Entypo,} from "@expo/vector-icons";
 
-import styles from './style';
+import styles from "./style";
+import emptyScreenImage from "../../../assets/images/reminderScreenImage.png";
 
 const RemainderListScreen = () => {
   return (
-<SafeAreaView style={styles.mainContainer}>
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.imageContainer}>
+        <Image source={emptyScreenImage} style={styles.emptyScreenImage} />
+      </View>
 
-</SafeAreaView>
-  )
-}
+      <TouchableOpacity
+          style={styles.addButtonContainer}
+          onPress={() => submitHandler()}
+        >
+          <Entypo name="add-to-list" size={34} color="#fff" />
+        </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
 export default RemainderListScreen;
--- src/screens/remainderListScreen/style.js
+++ src/screens/remainderListScreen/style.js
@@ -5,7 +5,26 @@ const styles = StyleSheet.create({
         flex:1,
         paddingTop:Platform.OS ==="android"?40:0,
         backgroundColor:"#000"
-    }
+    },
+    imageContainer:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    emptyScreenImage:{
+        width:300,
+        height:300,
+        resizeMode:"stretch"
+    },
+      addButtonContainer:{
+        backgroundColor:"#00ff7f",  
+        padding:15,
+        borderRadius:50,
+        position:'absolute',
+        bottom:20,
+        right:20,
+      },
+
 });
 
 export default styles;
