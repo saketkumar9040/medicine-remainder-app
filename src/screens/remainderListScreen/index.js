383ec9d97c450244209f46250c53951e0cd41dd2
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-06 20:39:06 +0530

    feat - render remainder list in remainder list screen completed

--- src/screens/remainderListScreen/index.js
+++ src/screens/remainderListScreen/index.js
@@ -1,49 +1,110 @@
-import { View, Text, SafeAreaView, Image ,TouchableOpacity} from "react-native";
-import React,{useState,useEffect} from "react";
-import { Entypo,} from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Entypo } from "@expo/vector-icons";
 
 import styles from "./style";
 import emptyScreenImage from "../../../assets/images/reminderScreenImage.png";
+import headerImage from "../../../assets/images/reminderScreenHeaderLogo.png";
 import { useSelector } from "react-redux";
 import { postRequest } from "../../utils/apiCallsHandler";
+import { MaterialCommunityIcons } from "@expo/vector-icons/build/Icons";
 
-const RemainderListScreen = ({navigation}) => {
+const RemainderListScreen = ({ navigation }) => {
+  const storedUserData = useSelector((state) => state.auth.userData);
 
-   const storedUserData = useSelector(state =>state.auth.userData);
-
-  const [reminderList,setReminderList] =useState([]);
-  console.log(reminderList)
+  const [reminderList, setReminderList] = useState([]);
+  console.log(reminderList);
 
   const getReminderList = async () => {
     try {
-      const fetchList = await postRequest("getReminderList",{userId:storedUserData._id});
-      console.log(fetchList.data)
+      const fetchList = await postRequest("getReminderList", {
+        userId: storedUserData._id,
+      });
+      setReminderList(fetchList.data);
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  }
+  };
 
-  useEffect(()=>{
-      getReminderList();
-  },[])
+  useEffect(() => {
+    getReminderList();
+  }, []);
 
   return (
     <SafeAreaView style={styles.mainContainer}>
-    {
-      // SHOW FLATLIST IF REMINDER LIST IS AVAILABLE IN DATABSE ===================================>
-    }
+      {
+        // SHOW FLATLIST IF REMINDER LIST IS AVAILABLE IN DATABSE ===================================>
+        reminderList.length > 0 ? (
+          <>
+            <Image source={headerImage} style={styles.headerImage} />
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={reminderList}
+              renderItem={({ item, index }) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    style={styles.medicineCardContainer}
+                  >
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        gap: 10,
+                        alignItems: "center",
+                      }}
+                    >
+                      <MaterialCommunityIcons
+                        name="pill"
+                        size={24}
+                        color="#fff"
+                      />
+                      <Text style={styles.medicineName}>
+                        {item.medicineName}
+                      </Text>
+                    </View>
+                    <Text style={styles.timing}>
+                      Timing :-{"  "} {new Date(item.time).getHours()} :{" "}
+                      {new Date(item.time).getMinutes() < 10 ? `0${new Date(item.time).getMinutes()}`:
+                      new Date(item.time).getMinutes()
+                      }
+                    </Text>
+                    <Text style={styles.frequency}>
+                      Servings :-{"  "} {item.frequency}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <View style={styles.imageContainer}>
+              <Image
+                source={emptyScreenImage}
+                style={styles.emptyScreenImage}
+              />
+            </View>
+            <Text style={styles.addRemainderText}>
+              {" "}
+              Add remainder to never miss your scheduled medicine time
+            </Text>
 
-      <View style={styles.imageContainer}>
-        <Image source={emptyScreenImage} style={styles.emptyScreenImage} />
-      </View>
-      <Text style={styles.addRemainderText}> Add remainder to never miss your scheduled medicine time</Text>
-        
-      <TouchableOpacity
-          style={styles.addButtonContainer}
-          onPress={() => navigation.navigate("Add Remainder")}
-        >
-          <Entypo name="add-to-list" size={34} color="#fff" />
-        </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.addButtonContainer}
+              onPress={() => navigation.navigate("Add Remainder")}
+            >
+              <Entypo name="add-to-list" size={34} color="#fff" />
+            </TouchableOpacity>
+          </>
+        )
+      }
     </SafeAreaView>
   );
 };
--- src/screens/remainderListScreen/style.js
+++ src/screens/remainderListScreen/style.js
@@ -1,38 +1,68 @@
 import { Platform, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    mainContainer:{
-        flex:1,
-        paddingTop:Platform.OS ==="android"?40:0,
-        backgroundColor:"#000"
-    },
-    imageContainer:{
-        marginVertical:80,
-        alignItems:"center",
-        justifyContent:"center"
-    },
-    emptyScreenImage:{
-        width:300,
-        height:300,
-        resizeMode:"stretch"
-    },
-    addButtonContainer:{
-        backgroundColor:"#00ff7f",  
-        padding:15,
-        borderRadius:50,
-        position:'absolute',
-        bottom:20,
-        right:20,
-      },
-      addRemainderText:{
-        fontSize:22,
-        fontWeight:"800",
-        letterSpacing:1,
-        color:"#fff",
-        textAlign:'center',
-        paddingHorizontal:50,
-      },
-
+  mainContainer: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 40 : 0,
+    backgroundColor: "#000",
+  },
+  imageContainer: {
+    marginVertical: 80,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  emptyScreenImage: {
+    width: 300,
+    height: 300,
+    resizeMode: "stretch",
+  },
+  addButtonContainer: {
+    backgroundColor: "#00ff7f",
+    padding: 15,
+    borderRadius: 50,
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+  addRemainderText: {
+    fontSize: 22,
+    fontWeight: "800",
+    letterSpacing: 1,
+    color: "#fff",
+    textAlign: "center",
+    paddingHorizontal: 50,
+  },
+  medicineCardContainer: {
+    margin: 10,
+    marginTop:20,
+    padding:20,
+    marginHorizontal:20,
+    borderWidth: 1,
+    borderColor: "#fff",
+    borderRadius:10,
+  },
+  headerImage: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+    resizeMode: "center",
+  },
+  medicineName:{
+    fontSize:22,
+    fontWeight:"800",
+    color:"#fff",
+  },
+  timing:{
+    fontSize:22,
+    fontWeight:"300",
+    color:"#fff",
+    marginVertical:10,
+  },
+  frequency:{
+    fontSize:18,
+    fontWeight:"300",
+    color:"#fff",
+  }
 });
 
 export default styles;
